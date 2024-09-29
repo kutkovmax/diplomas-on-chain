5c6f331d209605cbea10091c1bc794455ea4b1f7
Maxim <maximkutkov@gmail.com>
2024-09-29 18:23:31 +0300

    create certificate model

--- smart_diplomas/certificates/models.py
+++ smart_diplomas/certificates/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Certificate(models.Model):
+    student_name = models.CharField(max_length=100)
+    course_name = models.CharField(max_length=100)
+    issued_at = models.DateTimeField(auto_now_add=True)
+    blockchain_address = models.CharField(max_length=255)
+    certificate_hash = models.CharField(max_length=66)
+
+
+    def __str__(self):
+        return  f'{self.student_name} - {self.course_name}'
