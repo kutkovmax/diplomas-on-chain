b4a217ef0b25e818f393a2131192e0a5e245e0a3
Maxim <maximkutkov@gmail.com>
2024-09-30 18:21:46 +0300

    little fix model

--- smart_diplomas/certificates/models.py
+++ smart_diplomas/certificates/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 
-from django.db import models
-
 class Certificate(models.Model):
+    cert_id = models.CharField(max_length=66, primary_key=True)
     student_name = models.CharField(max_length=100)
     course_name = models.CharField(max_length=100)
     issued_at = models.DateTimeField(auto_now_add=True)
-    blockchain_address = models.CharField(max_length=255)
-    certificate_hash = models.CharField(max_length=66)
-
 
     def __str__(self):
-        return  f'{self.student_name} - {self.course_name}'
+        return f'{self.student_name} - {self.course_name}'
